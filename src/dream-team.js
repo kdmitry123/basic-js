49f112be3f4930b76c530c719fffb04ea3c4c93e
kdmitry123 <60309700+kdmitry123@users.noreply.github.com>
2020-02-21 13:52:48 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,20 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let dreamName = members.reduce((dreamTeam, member) => {
+    if (typeof member === "string") {
+      dreamTeam = [
+        ...dreamTeam,
+        member
+          .trim()
+          .charAt(0)
+          .toUpperCase()
+      ];
+      return dreamTeam;
+    }
+
+    return dreamTeam;
+  }, []);
+
+  return dreamName.sort().join("");
 };
